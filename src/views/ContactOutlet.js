3b3abafdf15bef6315162421fde372fce84bb9a2
blackMBP <ironhighh@gmail.com>
2021-03-14 20:15:30 +0700

    add load img

--- src/views/ContactOutlet.js
+++ src/views/ContactOutlet.js
@@ -1,37 +1,39 @@
-import React, { useEffect, useState } from 'react'
-import Axios from 'axios'
-import DataGridDevExtreme from '../components/DataGridDevExtreme'
-import '../config'
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import DataGridDevExtreme from "../components/DataGridDevExtreme";
+import "../config";
 
 export default function ContactOutlet() {
-  const [data, setData] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState(false)
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     async function callAPI() {
-      setIsLoading(true)
-      const url = `${global.config.apiUrl}/emps/outlets`
+      setIsLoading(true);
+      const url = `${global.config.apiUrl}/emps/outlets`;
       try {
         const { data } = await Axios(url);
-        setData(data[0])
-        setIsLoading(false)
-      }
-      catch (e) {
+        setData(data[0]);
+        setIsLoading(false);
+      } catch (e) {
         console.error("ContactHQAPI Error: ", e);
-        setError(e)
+        setError(e);
+        setIsLoading(false);
       }
     }
-    callAPI()
-  }, [])
+    callAPI();
+  }, []);
 
   return (
-    <div>
-      {error && <div>Something went wrong...</div>}
-      {isLoading ? (<h1>loading...</h1>) :
-        (
-          <DataGridDevExtreme data={data} />
-        )}
+    <div style={{ margin: 20 }}>
+      {isLoading && (
+        <img src={window.location.origin + "/load.gif"} alt="loading" />
+      )}
+      {error && (
+        <h3>Something went wrong... Please Connect VPN or contact IT.</h3>
+      )}
+      {!isLoading && !error && <DataGridDevExtreme data={data} />}
     </div>
-  )
+  );
 }
